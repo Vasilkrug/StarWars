4d988b4c57d4d83abfd7e8738f55cadd039e14db
vasilkrug <vkruglovec@gmail.com>
2023-05-24 20:59:18 +0300

    add page Home

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.scss';
+import Logo from "../../components/logo/Logo";
+
+const Home = () => {
+    return (
+        <div className={'page home-page'}>
+            <Logo/>
+
+        </div>
+    );
+};
+
+export default Home;
