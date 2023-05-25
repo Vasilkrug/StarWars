9499ff951d594d50ca43cd788aa2f2267204ecb5
vasilkrug <vkruglovec@gmail.com>
2023-05-25 21:53:27 +0300

    change Logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -4,7 +4,7 @@ import './Logo.scss';
 const Logo = () => {
     return (
         <div className={'logo'}>
-            <img src={require('../../assets/icons/logo.png')} alt=""/>
+            <img src={require('../../assets/icons/logo2.png')} alt=""/>
             <p>Discovery</p>
         </div>
     );
