d1581ac5f77a89010810d4eee5ff74b504131ffa
vasilkrug <vkruglovec@gmail.com>
2023-06-20 22:23:05 +0300

    add page Starships

---
+++ src/pages/starships/Starships.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Staeships.scss';
+import {CardList} from "../../components";
+
+const Starships = () => {
+    return (
+        <div>
+            <CardList title={'starships'}/>
+        </div>
+    );
+};
+
+export default Starships;
