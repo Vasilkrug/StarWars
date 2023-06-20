a7e8e421f47116bed796adc3bfdbe6249dd15e21
vasilkrug <vkruglovec@gmail.com>
2023-06-20 22:23:15 +0300

    add page Vehicles

---
+++ src/pages/vehicles/Vehicles.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Vehicles.scss';
+import {CardList} from "../../components";
+
+const Vehicles = () => {
+    return (
+        <div>
+            <CardList title={'vehicles'}/>
+        </div>
+    );
+};
+
+export default Vehicles;
