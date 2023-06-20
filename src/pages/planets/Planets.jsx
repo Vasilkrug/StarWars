0e3f22e7a1806e9c39cce46d4fb948d06de00bc1
vasilkrug <vkruglovec@gmail.com>
2023-06-20 22:22:52 +0300

    add page Planets

---
+++ src/pages/planets/Planets.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Planets.scss';
+import {CardList} from "../../components";
+
+const Planets = () => {
+    return (
+        <div>
+            <CardList title={'planets'}/>
+        </div>
+    );
+};
+
+export default Planets;
