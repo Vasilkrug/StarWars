145ff8a52e342f1108c043f40c23d22c0b131f5c
vasilkrug <vkruglovec@gmail.com>
2023-06-22 21:11:51 +0300

    add pagination

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -4,27 +4,40 @@ import {EMPTY_IMG_LINK, IMG_URL} from "../../api/constants";
 import {getQueryNumber} from "../../api/helpers";
 
 const CardList = ({title}) => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [itemsToShow, setItemsToShow] = useState(10);
+
+    const showMore = () => {
+        setItemsToShow(prevState => prevState + 10)
+    }
+
     useEffect(() => {
         const storage = localStorage.getItem('data')
         setData(JSON.parse(storage)[title])
     }, [])
 
     return (
-        <ul className={'card-list'}>
-            {data.map(item => {
-                return (
-                    <li key={item.name} className={'card-item'}>
-                        <h3>{item.name}</h3>
-                        <img
-                            src={`${IMG_URL}${title}/${getQueryNumber(item.url)}.jpg`}
-                            alt=""
-                            onError={(e) => e.target.src = EMPTY_IMG_LINK}
-                        />
-                    </li>
-                )
-            })}
-        </ul>
+        <div className={'card-list-container'}>
+            <ul className={'card-list'}>
+                {data.slice(0, itemsToShow).map(item => {
+                    return (
+                        <li key={item.name} className={'card-item'}>
+                            <h3>{item.name}</h3>
+                            <img
+                                src={`${IMG_URL}${title}/${getQueryNumber(item.url)}.jpg`}
+                                alt={`${title} img`}
+                                onError={(e) => e.target.src = EMPTY_IMG_LINK}
+                            />
+                        </li>
+                    )
+                })}
+            </ul>
+            {itemsToShow >= data.length ? null :
+                <button onClick={showMore}
+                        className={'show-more'}>
+                    Show More
+                </button>}
+        </div>
     );
 };
 
