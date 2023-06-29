a4833f4ac8fe348f4d80b615f9fd4d1ff645db3f
vasilkrug <vkruglovec@gmail.com>
2023-06-29 22:13:37 +0300

    add component BreadCrumbs

---
+++ src/components/breadCrumbs/BreadCrumbs.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import './BreadCrumbs.scss';
+import {Link, useLocation} from "react-router-dom";
+
+const BreadCrumbs = () => {
+    const location = useLocation()
+    let activeCrumbs = ''
+    const crumbs = location.pathname.split('/')
+        .filter(crumb => crumb !== '')
+        .map((crumb, index, array) => {
+            activeCrumbs += `/${crumb}`
+            if (index === 0) {
+                return (
+                    <>
+                        <div className={'crumb'}>
+                            <Link to={'/'}>home</Link>
+                        </div>
+                        <div className={'crumb'} key={crumb}>
+                            {index === array.length - 1 ?
+                                <p>{crumb}</p>
+                                :
+                                <Link to={activeCrumbs}>{crumb}</Link>
+                            }
+                        </div>
+                    </>
+                )
+            }
+            return (
+                <div className={'crumb'} key={crumb}>
+                    {index === array.length - 1 ?
+                        <p>{crumb}</p>
+                        :
+                        <Link to={activeCrumbs}>{crumb}</Link>
+                    }
+                </div>
+            )
+        })
+
+    return (
+        <>
+            {crumbs.length ?
+                <div className={'breadcrumbs'}>
+                    {crumbs}
+                </div>
+                : null
+            }
+        </>
+    );
+};
+
+export default BreadCrumbs;
