37e936187ed80613de3d22a1bf58e44696e77e07
vasilkrug <vkruglovec@gmail.com>
2023-07-09 13:44:35 +0300

    add fetch to api

--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,20 +1,19 @@
-import React, {useEffect, useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {SliderButtons} from '../index';
-import {EMPTY_IMG_LINK} from '../../api/constants';
+import {EMPTY_IMG_LINK, SWAPI_ROOT} from '../../api/constants';
 import {getQueryNumber} from '../../api/helpers';
-import './Slider.scss';
 import 'swiper/css';
+import {getSpecifiedData} from '../../api/api';
+import {Loader} from '../index';
+import './Slider.scss';
 
 
-const Slider = ({url, title, data}) => {
+const Slider = ({url, title, data, query}) => {
     const [currentData, setCurrentData] = useState([]);
-    const sliderRef = useRef(null)
 
     useEffect(() => {
-        const data = localStorage.getItem('data')
-        setCurrentData(JSON.parse(data)[`${title.toLowerCase()}`])
+        getSpecifiedData(`${SWAPI_ROOT}${query || title}`)
+            .then(data => setCurrentData(data.flat()))
     }, [])
 
     useEffect(() => {
@@ -24,25 +23,32 @@ const Slider = ({url, title, data}) => {
     }, [])
 
     return (
-        <div ref={sliderRef} className={`slider ${title.toLowerCase()} box-shadow`}>
-            <h2>{title}</h2>
-            <div className={'my-slider'}>
-                {data.slice(0, 5).map((item, index) => {
-                    return (
-                        <div key={item} className={'slide'}>
-                            <Link to={`/${title.toLowerCase()}/${item}`}
-                                  state={{...currentData[index], title: title.toLowerCase(), img: `${url}/${item}.jpg`}}
-                            >
-                                <h2>{currentData[index]?.name}</h2>
-                            </Link>
+        <div className={`slider ${title} box-shadow`}>
+            {!currentData.length ? <Loader/> :
+                <>
+                    <h2>{title}</h2>
+                    <div className={'my-slider'}>
+                        {data.slice(0, 5).map((item, index) => {
+                            return (
+                                <div key={item} className={'slide'}>
+                                    <Link to={`/${title}/${item}`}
+                                          state={{
+                                              ...currentData[index],
+                                              title: title,
+                                              img: `${url}/${item}.jpg`
+                                          }}
+                                    >
+                                        <h2>{currentData[index]?.name}</h2>
+                                    </Link>
 
-                            <img onError={(e) => e.target.src = EMPTY_IMG_LINK}
-                                 src={`${url}/${item}.jpg`}
-                                 alt='slide image'/>
-                        </div>
-                    )
-                })}
-            </div>
+                                    <img onError={(e) => e.target.src = EMPTY_IMG_LINK}
+                                         src={`${url}/${item}.jpg`}
+                                         alt='slide image'/>
+                                </div>
+                            )
+                        })}
+                    </div>
+                </>}
         </div>
     );
 };
