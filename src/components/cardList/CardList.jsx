46604424d40ba0edfa1648cf7efd0cd6767683d4
vasilkrug <vkruglovec@gmail.com>
2023-07-10 22:01:04 +0300

    added filtering by name

--- src/components/cardList/CardList.jsx
+++ src/components/cardList/CardList.jsx
@@ -1,29 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import {Loader, Card} from '../index';
+import React, {useEffect, useMemo, useState} from 'react';
+import {Loader, Card, SearchForm} from '../index';
 import {SWAPI_ROOT} from '../../api/constants';
 import {getSpecifiedData} from '../../api/api';
 import './CardList.scss';
 
 const CardList = ({title, img}) => {
     const [data, setData] = useState([]);
+    const [inputValue, setInputValue] = useState('')
     const [itemsToShow, setItemsToShow] = useState(12);
+
+    const inputHandler = (e) => {
+        setInputValue(e.target.value)
+    }
 
     const showMore = () => {
         setItemsToShow(prevState => prevState + 12)
     }
 
+    const filteredData = useMemo(() => {
+        return data.filter(({name}) => name.toLowerCase().includes(inputValue.toLowerCase()))
+    }, [inputValue, data])
+
     useEffect(() => {
         getSpecifiedData(`${SWAPI_ROOT}${title}`)
             .then(data => setData(data.flat()))
-    }, [])
+    }, [title])
 
     return (
         <>
             {!data.length ? <Loader/>
                 :
                 <div className={'card-list-container'}>
+                    <SearchForm inputValue={inputValue}
+                                onChange={inputHandler}
+                                onClick={setInputValue}/>
                     <ul className={'card-list'}>
-                        {data.slice(0, itemsToShow).map((item, index) => {
+                        {filteredData.slice(0, itemsToShow).map((item, index) => {
                             return <Card key={item.name}
                                          title={title}
                                          data={data}
@@ -33,7 +45,8 @@ const CardList = ({title, img}) => {
                                          index={index}/>
                         })}
                     </ul>
-                    {itemsToShow >= data.length ? null :
+                    {!filteredData.length ? <h2>Items not found</h2> : null}
+                    {itemsToShow >= filteredData.length ? null :
                         <button onClick={showMore}
                                 className={'show-more'}>
                             Show More
