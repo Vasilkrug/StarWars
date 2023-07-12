b52fc00b337c9273d55fbb71164a07c903da97cd
vasilkrug <vkruglovec@gmail.com>
2023-07-12 20:51:21 +0300

    change styles

--- src/components/filmSlider/FilmSlider.jsx
+++ src/components/filmSlider/FilmSlider.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import {useSelector} from 'react-redux';
 import {Swiper, SwiperSlide} from 'swiper/react';
 import {FilmSlide, Loader, SliderButtons} from '../index'
